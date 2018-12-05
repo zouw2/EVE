cfb43dfc3a648efa4a345e6822c7656e784133a1
Chun-Chieh Chang - Regular <changc25@rescomp5105.gene.com>
2018-12-05 10:16:03 -0800

    upload sbatch files

---
+++ tests/sbatch_rfsrc.R
@@ -0,0 +1,49 @@
+## Instructions ##
+## For Python engines: this script must be ran using command line 
+# "module load apps/python"
+# "Rscript sbatch_xxx.R"
+#
+# For R engines: this script can be run in http://rescomp5105.gene.com:8080
+# just open this script on RStudio and then click "Source" button 
+#
+
+################
+## User Input ##
+################
+runSpec <- list(
+  seed_base = 1000,
+  num_seeds = 2,
+
+  project_home = "~/EVE/tests",
+  engine = "rfeSRCC.r", ## ML engine
+  project_name = "rfsrc_outCV_test",  
+  training_data = "data/test_regr_surv_tcga_brca.csv", 
+  label_name = NA, #"pam50_RNAseq", 
+  sample_ID = "Patient_ID", 
+  surv_col = "os_time", #NA, 
+  event_col = "os_event", #NA, 
+  num_CV = 3, # -1 for LOOCV
+  RFE_step = 10, 
+  
+  ## change the weight of positive label
+  scale_pos_weight = 1, 
+  RFE_criteria = 'permute',  
+  
+  split_CVs = T,
+  queue_priority = "short" ## short, medium, long
+  
+)
+#######################
+### Input Data Processing (optional) ###
+#######################
+
+## modify data
+
+#######################
+## End of User Input ##
+#######################
+source("~/EVE/eve/submit/utils/sbatch_submit.R")
+sbatch_submit(runSpec)
+
+
+
---
+++ tests/sbatch_xgb_regression.R
@@ -0,0 +1,45 @@
+## Instructions ##
+## For Python engines: this script must be ran using command line 
+# "module load apps/python"
+# "Rscript ~/EVE/tests/sbatch_xgb_regression.R"
+
+################
+## User Input ##
+################
+runSpec <- list(
+  seed_base = 1000,
+  num_seeds = 2,
+
+  project_home = "~/EVE/tests",
+  engine = "XGBoost_regr.py", 
+  project_name = "xgboost_regression_outCV_test",  
+  training_data = "data/test_regr_surv_tcga_brca.csv", 
+  label_name = "os_time", 
+  sample_ID = "Patient_ID", 
+  surv_col = NA, 
+  event_col = NA, 
+  num_CV = 3, # -1 for LOOCV
+  RFE_step = 10, 
+  
+  ## change the weight of positive label
+  scale_pos_weight = 1, 
+  RFE_criteria = 'gain',  
+  
+  split_CVs = T,
+  queue_priority = "short" ## short, medium, long
+  
+)
+#######################
+### Input Data Processing (optional) ###
+#######################
+
+## modify data
+
+#######################
+## End of User Input ##
+#######################
+source("~/EVE/eve/submit/utils/sbatch_submit.R")
+sbatch_submit(runSpec)
+
+
+
